4052077faeef019295e270a8ab39e97b29e0989f
Kirill Yurkin <ugo0zapad@yandex.ru>
2024-01-23 19:31:22 +0300

    Forget to register base classes from game module

--- source/solunar/shockgame/shockgame.cpp
+++ source/solunar/shockgame/shockgame.cpp
@@ -6,6 +6,17 @@
 
 #include "shockgame/shockplayercontroller.h"
 
+// Base game includes
+#include "game/gamelogic/camera/cameramanager.h"
+#include "game/gamelogic/mainmenu/mainmenuworldcomponent.h"
+#include "game/gamelogic/player/playercontrollercomponent.h"
+#include "game/gamelogic/player/weaponchaincomponent.h"
+#include "game/gamelogic/saveload/saveloadmanager.h"
+#include "game/gamelogic/weapons/weaponcomponent.h"
+#include "game/gamelogic/weapons/weaponautocomponent.h"
+#include "game/gamelogic/weapons/weaponpistolcomponent.h"
+#include "game/gamelogic/world/levelmanager.h"
+
 namespace engine
 {
 
@@ -17,6 +28,17 @@ IGameInterface* g_gameInterface = (IGameInterface*)&s_shockGameInterface;
 ShockGameInterface* engine::getShockGameInterface()
 {
 	return &s_shockGameInterface;
+}
+
+void registerGameClasses()
+{
+	MainMenuWorldComponent::registerObject();
+	PlayerControllerComponent::registerObject();
+	WeaponComponent::registerObject();
+	WeaponAutoComponent::registerObject();
+	WeaponPistolComponent::registerObject();
+	WeaponChainComponent::registerObject();
+	LevelManagerComponent::registerObject();
 }
 
 void registerShockClasses()
@@ -36,7 +58,12 @@ ShockGameInterface::~ShockGameInterface()
 
 void ShockGameInterface::initialize()
 {
+	Core::msg("Initializing game");
+
 	// register game objects
+	registerGameClasses();
+
+	// register shock objects
 	registerShockClasses();
 
 	// add event listener
