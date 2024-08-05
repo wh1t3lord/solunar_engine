e47105bc72a4df620b0a811a7168e9a702085402
Kirill Yurkin <47507219+ugozapad@users.noreply.github.com>
2024-08-05 16:25:46 +0300

    Update shockplayercontroller.cpp

--- source/solunar/shockgame/shockplayercontroller.cpp
+++ source/solunar/shockgame/shockplayercontroller.cpp
@@ -210,7 +210,8 @@ void ShockPlayerController::updateCamera(float dt)
 	 //m_weaponEntity->setRotation(m_camera->getDirection());
 
 	glm::quat rot = glm::eulerAngleYX(glm::radians(-m_camera->m_yaw), glm::radians(m_camera->m_pitch));
-	m_weaponEntity->setRotation(glm::slerp(rot, m_weaponEntity->getRotation(), 55.0f * dt));
+	//m_weaponEntity->setRotation(glm::slerp(rot, m_weaponEntity->getRotation(), 55.0f * dt));
+	m_weaponEntity->setRotation(rot);
 
 #if 0
 	 glm::vec3 cameraDirection = CameraProxy::getInstance()->getDirection();
